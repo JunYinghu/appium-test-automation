8e10b7b384a186f742aaca811da57e75679cd009
hujy <hujy11@gmail.com>
2017-08-04 18:19:28 +0800

    read me for script.
    Signed-off-by: hujy <hujy11@gmail.com>

--- src/TestScript_1.py
+++ src/TestScript_1.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import time
 from time import sleep
 
 import unittest
@@ -11,22 +13,25 @@ PATH = lambda p: os.path.abspath(
 )
 
 class SimpleAndroidTests(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '6.0.1'
         desired_caps['deviceName'] = 'Galaxy Note5'
         desired_caps['appPackage'] = 'com.android.chrome'
         desired_caps['appActivity'] = 'com.google.android.apps.chrome.Main'
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-    def tearDown(self):
+        cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    
+    @classmethod
+    def tearDownClass(cls):
         # end the session
-        self.driver.quit()
-
-    def test_find_elements(self):
+        cls.driver.quit()
+        
+    def test_00_find_elements(self):
         el = self.driver.find_element_by_id('terms_accept')
         el.click()
+		
         # el = self.driver.find_element_by_accessibility_id('Arcs')
         # self.assertIsNotNone(el)
 
@@ -39,7 +44,17 @@ class SimpleAndroidTests(unittest.TestCase):
         # self.assertGreaterEqual(12, len(els))
 
         # self.driver.find_element_by_android_uiautomator('text("API Demos")')
-
+		
+    def test_01_confirm_terms_accept(self):
+        el = self.driver.find_element_by_id('signin_title')
+        self.assertEqual(el.text, "Sign in to Chrome", "failed")
+        el = self.driver.find_element_by_id('negative_button')
+        el.click()        
+        
+    def text_02_send_search_kay(self): 
+        time.sleep(2.0)
+        el = self.driver.find_element_by_id('search_box_text')
+        el.send_keys("hello")
 
     #def test_simple_actions(self):
     #    el = self.driver.find_element_by_accessibility_id('Graphics')
@@ -53,4 +68,5 @@ class SimpleAndroidTests(unittest.TestCase):
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SimpleAndroidTests)
+    suite.sortTestMethodsUsing=None
     unittest.TextTestRunner(verbosity=2).run(suite)
