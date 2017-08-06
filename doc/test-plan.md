651db7ffa710785fc618994c7692ba6879f0e3ed
hujy <hujy11@gmail.com>
2017-08-06 10:51:31 +0800

    to test chrome browser on android device
    Signed-off-by: hujy <hujy11@gmail.com>

--- doc/test-plan.md
+++ doc/test-plan.md
@@ -1,26 +1,29 @@
 # Chrome Android Test Automation Plan
 
-1  This project is designed to make more pratice on automation testing.
+1  This project is designed to make more practice on automation testing.
 
 2  This automation test scripts were written in Python.
 
-3  There are 3 functions to be tested
+3  There are 5 functions to be tested
 
    - Opens Chrome browser on Android device
    - Search Accept&continue button via find_element_by_id,
-     Click the button going to welcome page
+     Click the button going to user email registration page
    - If text matches 'sing in...' then user to leave the page user does not want
      Clicks 'No Thanks' button to leave
   
-   - Keys in search keyword in search box on default page (google screen)
+   - Set search keyword in search box on default page (google screen)
+     tap on search keyword via action move
+     
    - Exit from browser
+     Wait 10 second to confirm search result as the above
    
 4 Test tools / ENV
    - Notepad++
    - Python
    - Appium   
    
-5 Tested deivce information:
+5 Tested device information:
    - Smasung Note 5
    - Version 6.0.1
 
--- src/TestScript_1.py
+++ src/TestScript_1.py
@@ -3,14 +3,20 @@ import sys
 import time
 from time import sleep
 
+import glob
 import unittest
 
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from appium.webdriver.common.multi_action import MultiAction
+
 
 # Returns abs path relative to this file and not cwd
-PATH = lambda p: os.path.abspath(
-    os.path.join(os.path.dirname(__file__), p)
-)
+#PATH = lambda p: os.path.abspath(
+  #  os.path.join(os.path.dirname(__file__), p)
+#)
+
+SLEEPY_TIME = 1
 
 class SimpleAndroidTests(unittest.TestCase):
     @classmethod
@@ -24,11 +30,8 @@ class SimpleAndroidTests(unittest.TestCase):
         cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
     
     @classmethod
-    def tearDownClass(cls):
-        # end the session
-        cls.driver.quit()
         
-    def test_00_find_elements(self):
+    def test_00_confirm_terms_accept(self):
         el = self.driver.find_element_by_id('terms_accept')
         el.click()
 		
@@ -45,26 +48,43 @@ class SimpleAndroidTests(unittest.TestCase):
 
         # self.driver.find_element_by_android_uiautomator('text("API Demos")')
 		
-    def test_01_confirm_terms_accept(self):
+    def test_01_Reject_Register_email(self):
         el = self.driver.find_element_by_id('signin_title')
         self.assertEqual(el.text, "Sign in to Chrome", "failed")
         el = self.driver.find_element_by_id('negative_button')
         el.click()        
-        
-    def text_02_send_search_kay(self): 
-        time.sleep(2.0)
+    
+    def test_02_keyin_search_key(self): 
         el = self.driver.find_element_by_id('search_box_text')
-        el.send_keys("hello")
-
-    #def test_simple_actions(self):
-    #    el = self.driver.find_element_by_accessibility_id('Graphics')
-    #    el.click()
+        el.set_value('Search keywords')
+                
+    def test_03_send_search_key(self):     
+        el = self.driver.find_element_by_id('refine_view_id')
+        action = TouchAction(self.driver)
+        action.press(el, x=-10, y=0).tap().perform()
+        self.driver.current_context
+        self.driver.contexts        
+        #a1 = TouchAction()
+        #a1.press(el) \
+            #.move_to(x=-30, y=0) \
+            #.wait(3000) \
+            #.tap(a1).perform()
+        #self.driver.implicitly_wait(6000)
+        
+    def test_04_tearDownClass(cls):
+        #end the session
+        time.sleep(10)
+        cls.driver.quit()       
+ 
+        
+#def test_simple_actions(self):
+#    el = self.driver.find_element_by_accessibility_id('Graphics')
+#    el.click()
 #
- #       el = self.driver.find_element_by_accessibility_id('Arcs')
-  #      el.click()
+#    el = self.driver.find_element_by_accessibility_id('Arcs')
+#      el.click()
 
-   #     self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
-
+#     self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SimpleAndroidTests)
