393dbde65607ee620a35dcfc514ece58eb702cd0
hujy <hujy11@gmail.com>
2017-08-10 17:26:00 +0800

    update files Signed-off-by: hujy <hujy11@gmail.com>

--- src/TestScript_1.py
+++ src/TestScript_1.py
@@ -10,7 +10,6 @@ from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 from appium.webdriver.common.multi_action import MultiAction
 
-
 # Returns abs path relative to this file and not cwd
 #PATH = lambda p: os.path.abspath(
   #  os.path.join(os.path.dirname(__file__), p)
@@ -18,72 +17,67 @@ from appium.webdriver.common.multi_action import MultiAction
 
 SLEEPY_TIME = 1
 
+#Launch chrome browser in Samsung Android
 class SimpleAndroidTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '6.0.1'
+        desired_caps['platformVersion'] = '7.0'
         desired_caps['deviceName'] = 'Galaxy Note5'
         desired_caps['appPackage'] = 'com.android.chrome'
         desired_caps['appActivity'] = 'com.google.android.apps.chrome.Main'
         cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-    
     @classmethod
-        
+    
+    #accept terms
     def test_00_confirm_terms_accept(self):
+        print self.driver.current_activity
         el = self.driver.find_element_by_id('terms_accept')
         el.click()
-		
-        # el = self.driver.find_element_by_accessibility_id('Arcs')
+	    # el = self.driver.find_element_by_accessibility_id('Arcs')
         # self.assertIsNotNone(el)
-
         # self.driver.back()
-
-        # el = self.driver.find_element_by_accessibility_id("App")
         # self.assertIsNotNone(el)
-
         # els = self.driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
         # self.assertGreaterEqual(12, len(els))
-
         # self.driver.find_element_by_android_uiautomator('text("API Demos")')
-		
+	
+    #Ingore email register screen	
     def test_01_Reject_Register_email(self):
+        print self.driver.current_activity
         el = self.driver.find_element_by_id('signin_title')
         self.assertEqual(el.text, "Sign in to Chrome", "failed")
         el = self.driver.find_element_by_id('negative_button')
         el.click()        
     
+    #Key in search keywords
     def test_02_keyin_search_key(self): 
+        print self.driver.current_activity
         el = self.driver.find_element_by_id('search_box_text')
         el.set_value('Search keywords')
-                
+        #press enter to submit keywords.
+        #self.driver.keyevent(66)        
+        
+    #using touch_action to get context keywords
     def test_03_send_search_key(self):     
         el = self.driver.find_element_by_id('refine_view_id')
         action = TouchAction(self.driver)
         action.press(el, x=-10, y=0).tap().perform()
-        self.driver.current_context
-        self.driver.contexts        
-        #a1 = TouchAction()
-        #a1.press(el) \
-            #.move_to(x=-30, y=0) \
-            #.wait(3000) \
-            #.tap(a1).perform()
-        #self.driver.implicitly_wait(6000)
         
-    def test_04_tearDownClass(cls):
+    #Close chromme                   
+    def test_04_tearDownClass(self):
         #end the session
-        time.sleep(10)
-        cls.driver.quit()       
- 
-        
-#def test_simple_actions(self):
-#    el = self.driver.find_element_by_accessibility_id('Graphics')
+        #self.driver.keyevent(4)
+        time.sleep(8)
+        #print dir (self.driver)  - to get element
+        self.driver.quit()       
+     
+#    def test_simple_actions(self):
+#    el = self.driver.find_elemesnt_by_accessibility_id('Graphics')
 #    el.click()
-#
 #    el = self.driver.find_element_by_accessibility_id('Arcs')
 #      el.click()
-
 #     self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
 
 if __name__ == '__main__':
---
+++ src/TestScript_Phone.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import time
+from time import sleep
+
+import glob
+import unittest
+
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+from appium.webdriver.common.multi_action import MultiAction
+
+
+#Open contacts window in samsung android        
+class SimpleAndroidTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '7.0'
+        desired_caps['deviceName'] = 'Galaxy Note5'
+        desired_caps['appPackage'] = 'com.samsung.android.contacts'
+        desired_caps['appActivity'] = 'com.android.dialer.DialtactsActivity'
+        cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    @classmethod
+    
+    #Dail phone number and calick on call button
+    def test_01_dail_number(self):
+        self.driver.keyevent(15)
+        self.driver.keyevent(12)
+        self.driver.keyevent(10)
+        self.driver.keyevent(10)
+        self.driver.keyevent(8)
+        self.driver.keyevent(12)
+        self.driver.keyevent(7)
+        self.driver.keyevent(16)
+        self.driver.keyevent(5)
+            
+    #wait for 30 seconds, if no response, call will be end and Close contacts window    
+    def test_03_tearDownClass(self):
+        #end the session
+        time.sleep(30)
+        self.driver.keyevent(6)
+        #to get element
+        #print dir (self.driver)        
+        self.driver.quit()      
+        
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleAndroidTests)
+    suite.sortTestMethodsUsing=None
+    unittest.TextTestRunner(verbosity=2).run(suite)
